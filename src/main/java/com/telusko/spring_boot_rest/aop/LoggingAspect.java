898871aa7f5b272a1b387c099ae54ca5a51f8e5b
Anuj Sharma <anujsharmajan06@gmail.com>
2026-02-03 23:46:57 +0530

    Add method-level AOP logging using JoinPoint

--- src/main/java/com/telusko/spring_boot_rest/aop/LoggingAspect.java
+++ src/main/java/com/telusko/spring_boot_rest/aop/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.telusko.spring_boot_rest.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -12,8 +13,9 @@ public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
     // return type, class-name.method-name(args)
-    @Before("execution(* com.telusko.spring_boot_rest.service.JobService.*(..))")
-    public void logMethodCall(){
-        LOGGER.info("Method Called");
+    @Before("execution(* com.telusko.spring_boot_rest.service.JobService.getJob(..)) || " +
+            "execution(* com.telusko.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodCall(JoinPoint jp){
+        LOGGER.info("Method called " + jp.getSignature().getName());
     }
 }
